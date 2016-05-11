3df52a36c6f6c23bf8c96770557ce47c83d74f18
freemanoid <freemanoid321@gmail.com>
2016-05-11 20:35:54 +0300

    update

--- 4.rb
+++ 4.rb
@@ -1,1 +1,17 @@
-something
+source "https://rubygems.org"
+
+ruby '2.3.1'
+
+gem 'sinatra'
+gem 'sinatra-json'
+gem 'sinatra-reloader'
+
+gem 'sequel'
+gem 'sqlite3'
+
+gem 'octokit'
+gem 'git_diff_parser'
+
+gem 'pry-byebug'
+gem 'pry-alias'
+
